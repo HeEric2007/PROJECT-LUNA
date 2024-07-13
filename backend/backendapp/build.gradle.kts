bd93b2c7c8048535aea690646746bd313e4b15de
alexhuang0729 <ah4631227@gmail.com>
2024-07-12 23:15:48 -0500

    First commit

--- backend/backendapp/build.gradle.kts
+++ backend/backendapp/build.gradle.kts
@@ -9,11 +9,12 @@ version = "0.0.1-SNAPSHOT"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
+		languageVersion = JavaLanguageVersion.of(22)
 	}
 }
 
 repositories {
+	google()
 	mavenCentral()
 }
 
@@ -23,6 +24,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
--- backend/backendapp/src/main/java/com/example/backendapp/BackendappApplication.java
+++ backend/backendapp/src/main/java/com/example/backendapp/BackendappApplication.java
@@ -2,8 +2,10 @@ package com.example.backendapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class }) //take out excludes when we need database and security
 public class BackendappApplication {
 
 	public static void main(String[] args) {
