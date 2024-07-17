2b39d5c943369402b25ba766ddcbb5dc271be2b0
alexhuang0729 <ah4631227@gmail.com>
2024-07-16 20:25:22 -0500

    Fixed generation type

--- backend/backendapp/src/main/java/com/example/backendapp/User.java
+++ backend/backendapp/src/main/java/com/example/backendapp/User.java
@@ -7,7 +7,7 @@ import jakarta.persistence.*;
 public class User {
 
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE)
     private Long id;
     private String firstName;
     private String lastName;
